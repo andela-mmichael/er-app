0c4bcb1747367ad1bcebf3f8d06ce44885a3658c
andela-oadebayo <oluwadamilola.adebayo@andela.co>
2015-06-02 12:52:28 +0100

    Initial Commit Used composer to install doctrine Did the basic set up.

---
+++ ORM/config/config.php
@@ -0,0 +1,15 @@
+<?php
+//App configuration
+$dbParams = [
+    'dbname' => 'erapp',
+    'user' => 'root',
+    'password' => 'root',
+    'host' => 'localhost',
+    'driver' => 'pdo_mysql',
+    'unix_socket' =>'/Applications/MAMP/tmp/mysql/mysql.sock',
+    'port' => '8889'
+];
+
+//Dev mode?
+$dev = true;
+?>
